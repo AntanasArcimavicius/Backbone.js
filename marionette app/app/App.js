8281f2cdb0d48a1ef24e225d9913d891b8234e9c
Antanas Arcimavicius <antanas.arcimavicius@teliacompany.com>
2021-08-17 16:38:36 +0300

    installed faker

--- marionette app/app/App.js
+++ marionette app/app/App.js
@@ -1,10 +1,10 @@
 import Marionette from 'backbone.marionette';
-import ItemView from './views/RootView';
+import RootView from './views/RootView';
 
 export default Marionette.Application.extend({
   region: '#app',
 
   onStart() {
-    this.showView(new ItemView());
+    this.showView(new RootView());
   }
 });
